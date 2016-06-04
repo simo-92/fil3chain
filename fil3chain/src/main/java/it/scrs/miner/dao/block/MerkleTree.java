32d1b896e51c4264ca092ea455b7872ece6ee650
giordanocristini <giordano.cristini@gmail.com>
2016-06-04 11:57:06 +0200

    implementazione merkletree

---
+++ fil3chain/src/main/java/it/scrs/miner/dao/block/MerkleTree.java
@@ -0,0 +1,61 @@
+
+package it.scrs.miner.dao.block;
+import java.util.List;
+import it.scrs.miner.dao.transaction.Transaction;
+import java.util.ArrayList;
+/**
+ * classe per la costruzione di un merkletree
+ * @author giordanocristini
+ */
+public class MerkleTree {
+    /**
+     * metodo che costruisce il merkleTree a partire da una lista di hash di transazioni
+     * @param hashTransactions lista degli hash delle transazioni del blocco
+     * @return <b>merkleRoot</b> la radice del merkleTree
+     */
+    public static String buildMerkleTree(List<String> hashTransactions){
+        String merkleRoot="";
+        List<String> aux=new ArrayList();
+        aux.addAll(hashTransactions);
+        //finche' non rimane solo un nodo radice
+        while(aux.size()!=1){
+            for(int j=0;j<aux.size();j++){
+                
+                if(j+1==(aux.size())){ //se dispari accoppia con se stesso
+                    String n1=aux.get(j);
+                    aux.remove(j);
+                    aux.add(j, MerkleTree.joinNodes(n1, n1));
+                }else { //accoppia due node
+                    String n1=aux.get(j);
+                    String n2=aux.get(j+1);
+                    aux.remove(j); //rimuovi il nodo j
+                    aux.remove(j); //rimuovi il nodo j+1 che ora è il nodo j!!!
+                    aux.add(j, MerkleTree.joinNodes(n1, n2));
+                }
+            }
+            
+        }
+        merkleRoot=aux.get(0);
+        return merkleRoot;
+    }
+    /**
+     * metodo che fonde due nodi
+     * @param n1 prima nodo
+     * @param n2 secondo nodo (puo' essere uguale a <b>n1</b> se le transazioni sono dispari
+     * @return fusione tra n1 e n2 (hash)
+     */
+    private static String joinNodes(String n1, String n2){
+        //return hash(n1+n2); TODO
+        return n1+n2;
+    }
+    //test
+    public static void main(String[] args){
+        List<String> trans=new ArrayList();
+        trans.add("A");
+        trans.add("B");
+        trans.add("C");
+        trans.add("D");
+        trans.add("E");
+        System.out.println(MerkleTree.buildMerkleTree(trans));
+    }
+}
