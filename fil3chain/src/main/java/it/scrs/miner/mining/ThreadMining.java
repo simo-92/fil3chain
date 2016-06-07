40279a279fdb908fcc2031e813aaa2fb2cea37f5
Simone <simone9_2@hotmail.it>
2016-06-07 14:53:51 +0200

    tred mining classe

---
+++ fil3chain/src/main/java/it/scrs/miner/mining/ThreadMining.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.scrs.miner.mining;
+
+import it.scrs.miner.dao.block.Block;
+
+/**
+ *
+ * @author simone
+ */
+public class ThreadMining {
+   /* public static Block mineNewBlock(Object... fields){ 
+        /*come parametri ha i campi del blocco sono tutti object per essere generici
+          perchè tanto vanno hashati 
+          l'ordine è il seguente :
+          -
+          -
+          -
+        
+        
+        
+        
+    }*/
+}
